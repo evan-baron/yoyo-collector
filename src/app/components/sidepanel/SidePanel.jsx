c08f32ea999cde904f24207c19cd9bb6d3890d40
evan-baron <egbaron@gmail.com>
2025-04-11 16:15:48 -0600

    positioning modal and adjusting side panel

--- src/app/components/sidepanel/SidePanel.jsx
+++ src/app/components/sidepanel/SidePanel.jsx
@@ -20,18 +20,28 @@ function SidePanel() {
 		<section className={styles['side-panel']}>
 			<nav className={styles.nav}>
 				<ul className={styles.ul}>
-					<Link href='/'>Home</Link>
+					<Link className={styles.link} href='/'>
+						Home
+					</Link>
 					{user && (
 						<>
-							<Link href={`/${user.handle || user.id}/collections`}>
+							<Link
+								className={styles.link}
+								href={`/${user.handle || user.id}/collections`}
+							>
 								My Collection
 							</Link>
-							<Link href={`/${user.handle || user.id}/new`}>
+							<Link
+								className={styles.link}
+								href={`/${user.handle || user.id}/new`}
+							>
 								New Collection
 							</Link>
 						</>
 					)}
-					<Link href='/collections'>View Collections</Link>
+					<Link className={styles.link} href='/collections'>
+						View Collections
+					</Link>
 					<li className={styles.search}>
 						<label htmlFor='search'>Search</label>
 						<input name='search'></input>
