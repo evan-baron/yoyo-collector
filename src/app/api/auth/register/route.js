a37c3a8bba69078cd680d17dcd42951a94df110a
evan-baron <egbaron@gmail.com>
2025-04-14 12:13:14 -0600

    all register formatting completed, time for login

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,12 +1,66 @@
 import authService from '@/services/authService';
 const { register } = authService;
 import { NextResponse } from 'next/server';
+import validator from 'validator';
 
 export async function POST(req) {
 	try {
 		const { first, last, email, password } = await req.json();
 
-		const { user } = await register(first, last, email, password);
+		const firstName = first.trim();
+		const lastName = last.trim();
+		const emailTrimmed = email.trim();
+		const passwordTrimmed = password.trim();
+
+		const nameValid = /^[a-zA-Z\s'-]+$/;
+
+		if (!nameValid.test(firstName) || !nameValid.test(lastName)) {
+			const response = NextResponse.json(
+				{
+					message: 'Your name must only contain alphanumeric characters',
+				},
+				{ status: 400 }
+			);
+
+			return response;
+		}
+
+		if (!validator.isEmail(emailTrimmed)) {
+			const response = NextResponse.json(
+				{
+					message:
+						'Your email must be in valid email format. E.g.: email@domain.com',
+				},
+				{ status: 400 }
+			);
+
+			return response;
+		}
+
+		const passwordValid =
+			passwordTrimmed.length >= 8 &&
+			/[A-Z]/.test(password) &&
+			/\d/.test(password) &&
+			/[!@#$%^&*()_+{}\[\]:;"'<>,.?/|\\~]/.test(password);
+
+		if (!passwordValid) {
+			const response = NextResponse.json(
+				{
+					message:
+						'Your password must be at least 8 characters long, contain one uppercase character, and one special character',
+				},
+				{ status: 400 }
+			);
+
+			return response;
+		}
+
+		const { user } = await register(
+			firstName,
+			lastName,
+			emailTrimmed,
+			passwordTrimmed
+		);
 
 		const response = NextResponse.json(
 			{
--- src/app/components/loading/LoadingSpinner.jsx
+++ src/app/components/loading/LoadingSpinner.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import styles from './loadingSpinner.module.scss';
 
 function LoadingSpinner() {
-	return <div className={styles['loading-spinner']}></div>;
+	const [loadingText, setLoadingText] = useState('');
+
+	useEffect(() => {
+		const loadingStates = ['', '.', '..', '...'];
+		let index = 0;
+
+		const interval = setInterval(() => {
+			setLoadingText(loadingStates[index]);
+			index = (index + 1) % loadingStates.length; // Loop back to start
+		}, 500);
+
+		return () => clearInterval(interval); // Cleanup on unmount
+	}, []);
+
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.h2}>
+				Loading
+				<div className={styles.elipses}>{loadingText}</div>
+			</h2>
+			<div className={styles.spinner}>
+				<div className={styles['loading-spinner']}></div>
+			</div>
+		</div>
+	);
 }
 
 export default LoadingSpinner;
