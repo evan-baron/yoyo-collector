77139053a3f4e9f95497436944578ab7dc5402ef
evan-baron <egbaron@gmail.com>
2025-04-16 20:08:21 -0600

    added lot of fields to form

--- src/app/components/settingsConsole/profileSettings/ProfileSettings.jsx
+++ src/app/components/settingsConsole/profileSettings/ProfileSettings.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 // Libraries
-import React from 'react';
+import React, { useState } from 'react';
 
 // Styles
 import styles from './profileSettings.module.scss';
@@ -14,49 +14,160 @@ import { useAppContext } from '@/app/context/AppContext';
 
 function ProfileSettings({ setViewSettings }) {
 	const { user } = useAppContext();
-	const { first_name, last_name, handle, email } = user;
+	const {
+		first_name,
+		last_name,
+		handle,
+		favorite_yoyo,
+		favorite_brand,
+		country,
+		state,
+		city,
+		description,
+	} = user;
+
+	const location = () => {
+		return [city, state, country].filter(Boolean).join(', ');
+	};
+
+	const [formData, setFormData] = useState({
+		first: first_name,
+		last: last_name,
+		handle: handle,
+		yoyo: favorite_yoyo,
+		brand: favorite_brand,
+		location: location(),
+		description: description,
+	});
+	const [maxLength, setMaxLength] = useState(300);
+
 	console.log(user);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		console.log(name);
+		setFormData((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+
+		if (name === 'description') {
+			setMaxLength(300 - e.target.value.length);
+		}
+	};
 
 	return (
 		<div className={styles.profile}>
 			<h2 className={styles.h2}>Profile Settings</h2>
 			<form className={styles['profile-form']}>
-				<div className={`${styles['form-item']} ${styles['profile-data']}`}>
-					<label htmlFor='first' className={styles.label}>
-						First Name
-						<input type='text' placeholder='First' className={styles.input} />
-					</label>
-					<label htmlFor='last' className={styles.label}>
-						Last Name
-						<input type='text' placeholder='Last' className={styles.input} />
-					</label>
-					<label htmlFor='handle' className={styles.label}>
-						Handle
-						<input
-							type='text'
-							placeholder='Nickname'
-							className={styles.input}
-						/>
-					</label>
-					<label htmlFor='favorite' className={styles.label}>
-						Favorite Yoyo
-						<input
-							type='text'
-							placeholder='Mt Fuji Draupnir'
-							className={styles.input}
-						/>
-					</label>
-					<label htmlFor='location' className={styles.label}>
-						Location
-						<input type='text' placeholder='Earth' className={styles.input} />
-					</label>
+				<div className={styles.top}>
+					<div className={`${styles['form-item']} ${styles['profile-data']}`}>
+						<div className={styles.names}>
+							<div className={styles.item}>
+								<label htmlFor='first' className={styles.label}>
+									First Name
+								</label>
+								<input
+									type='text'
+									name='first'
+									placeholder='First'
+									className={styles.input}
+									value={formData.first}
+									onChange={handleChange}
+								/>
+							</div>
+							<div className={styles.item}>
+								<label htmlFor='last' className={styles.label}>
+									Last Name
+								</label>
+								<input
+									type='text'
+									name='last'
+									placeholder='Last'
+									className={styles.input}
+									value={formData.last}
+									onChange={handleChange}
+								/>
+							</div>
+						</div>
+						<div className={styles.item}>
+							<label htmlFor='handle' className={styles.label}>
+								Handle
+							</label>
+							<input
+								type='text'
+								name='handle'
+								placeholder='Nickname'
+								className={styles.input}
+								value={formData.handle}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className={styles.item}>
+							<label htmlFor='yoyo' className={styles.label}>
+								Favorite Yoyo
+							</label>
+							<input
+								type='text'
+								name='yoyo'
+								placeholder='Duncan Imperial'
+								className={styles.input}
+								value={formData.yoyo}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className={styles.item}>
+							<label htmlFor='brand' className={styles.label}>
+								Favorite Brand
+							</label>
+							<input
+								type='text'
+								name='brand'
+								placeholder='Duncan'
+								className={styles.input}
+								value={formData.brand}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className={styles.item}>
+							<label htmlFor='location' className={styles.label}>
+								Location
+							</label>
+							<input
+								type='text'
+								name='location'
+								placeholder='Earth'
+								className={styles.input}
+								value={formData.location}
+								onChange={handleChange}
+							/>
+						</div>
+					</div>
+					<div
+						className={`${styles['form-item']} ${styles['profile-picture']}`}
+					></div>
 				</div>
-				<div
-					className={`${styles['form-item']} ${styles['profile-picture']}`}
-				></div>
-				<div
-					className={`${styles['form-item']} ${styles['profile-summary']}`}
-				></div>
+				<div className={styles.bottom}>
+					<div
+						className={`${styles['form-item']} ${styles['profile-summary']}`}
+					>
+						<div className={styles.item}>
+							<label htmlFor='description' className={styles.label}>
+								Tell us about yourself
+							</label>
+							<textarea
+								name='description'
+								id='description'
+								className={styles.textarea}
+								maxLength={300}
+								placeholder='I like long walks on the beach, throwing yoyos in the rain, and walking the dog with expensive yoyos.'
+								onChange={handleChange}
+								value={formData.description}
+							></textarea>
+							<div className={styles['max-length']}>{maxLength}</div>
+						</div>
+					</div>
+				</div>
 			</form>
 			<div
 				className={styles['view-profile']}
