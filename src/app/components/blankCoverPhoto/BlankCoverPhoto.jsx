ee3152b70be908be0d99841a2df22b134335f21c
evan-baron <egbaron@gmail.com>
2025-04-25 17:38:40 -0600

    added blank cover photo component to collection template

--- src/app/components/blankCoverPhoto/BlankCoverPhoto.jsx
+++ src/app/components/blankCoverPhoto/BlankCoverPhoto.jsx
@@ -1,7 +1,21 @@
+// Libraries
 import React from 'react';
 
+// Styles
+import styles from './blankCoverPhoto.module.scss';
+
 function BlankCoverPhoto() {
-	return <div>BlankCoverPhoto</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.frame}>
+				<div className={styles.landscape}>
+					<div className={styles.circle}></div>
+					<div className={styles.square1}></div>
+					<div className={styles.square2}></div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default BlankCoverPhoto;
