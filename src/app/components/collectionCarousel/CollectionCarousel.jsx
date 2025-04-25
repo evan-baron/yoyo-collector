587ff41091fc3923d463e39b3780953fbb38ee8d
evan-baron <egbaron@gmail.com>
2025-04-24 19:43:38 -0600

    styling and conditional

--- src/app/components/collectionCarousel/CollectionCarousel.jsx
+++ src/app/components/collectionCarousel/CollectionCarousel.jsx
@@ -7,7 +7,7 @@ import styles from './collectionCarousel.module.scss';
 function CollectionCarousel({ type }) {
 	return (
 		<div className={styles.collection}>
-			<h2 className={styles.h2}>{type}</h2>
+			{type && <h2 className={styles.h2}>{type}</h2>}
 			<div className={styles['scroll-wrapper']}>
 				<div className={styles.carousel}>
 					<div className={styles.tile}></div>
