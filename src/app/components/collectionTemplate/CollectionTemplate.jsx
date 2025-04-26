15d2b95883c7b8416ae43758326324d8341a425d
evan-baron <egbaron@gmail.com>
2025-04-26 17:51:09 -0600

    added to collection template

--- src/app/components/collectionTemplate/CollectionTemplate.jsx
+++ src/app/components/collectionTemplate/CollectionTemplate.jsx
@@ -1,15 +1,16 @@
 // Libraries
 import React from 'react';
 import Link from 'next/link';
+import dayjs from 'dayjs';
 
 // Styles
 import styles from './collectionTemplate.module.scss';
 
 // MUI
-import { Edit } from '@mui/icons-material';
+import { Edit, FavoriteOutlined } from '@mui/icons-material';
 
 // Components
-import PictureUploader from '../pictureUploader/PictureUploader';
+import BlankCoverPhoto from '../blankCoverPhoto/BlankCoverPhoto';
 
 function CollectionTemplate({ collection }) {
 	const {
@@ -24,11 +25,36 @@ function CollectionTemplate({ collection }) {
 	} = collection;
 
 	const settingsLink = `/mycollections/${id}/settings`;
+	const created = dayjs(created_at).format('MMMM, D, YYYY');
 
 	return (
 		<div className={styles['collection-container']}>
 			<div className={styles.title}>
 				<h1 className={styles.h1}>{collection_name}</h1>
+				<div className={styles.details}>
+					<h3 className={styles.h3}>Created {created}</h3>
+					<p className={styles.likes}>
+						<svg viewBox='0 0 24 24' className={styles.heart}>
+							<defs>
+								<linearGradient
+									id='quoteGradient'
+									x1='0%'
+									y1='0%'
+									x2='100%'
+									y2='100%'
+								>
+									<stop offset='0%' stopColor='#ff00ff' />
+									<stop offset='90%' stopColor='#00e1ff' />
+								</linearGradient>
+							</defs>
+							<path
+								d='m12 21.35-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54z'
+								fill='url(#quoteGradient)'
+							/>
+						</svg>
+						{likes ? likes : '69'} likes
+					</p>
+				</div>
 				<p className={styles.description}>
 					{collection_description
 						? collection_description
@@ -40,7 +66,8 @@ function CollectionTemplate({ collection }) {
 					<div className={styles.left}>
 						<h2 className={styles.h2}>Cover Photo</h2>
 						<div className={styles.cover}>
-							<PictureUploader uploadType='cover' />
+							{/* Collection photo or default photo */}
+							<BlankCoverPhoto />
 						</div>
 					</div>
 					<div className={styles.right}>
