fe6d79f835ee3707d454ed9e7f5208f06652b3df
evan-baron <egbaron@gmail.com>
2025-04-26 10:56:05 -0600

    filled out 70% of collection template

--- src/app/components/collectionTemplate/CollectionTemplate.jsx
+++ src/app/components/collectionTemplate/CollectionTemplate.jsx
@@ -15,6 +15,7 @@ function CollectionTemplate({ collection }) {
 	const {
 		id,
 		collection_name,
+		collection_description,
 		likes,
 		created_at,
 		handle,
@@ -26,10 +27,57 @@ function CollectionTemplate({ collection }) {
 
 	return (
 		<div className={styles['collection-container']}>
+			<div className={styles.title}>
+				<h1 className={styles.h1}>{collection_name}</h1>
+				<p className={styles.description}>
+					{collection_description
+						? collection_description
+						: 'This is a sample description. This collection only contains solid gold and platinum yoyos. All of which are worth millions.'}
+				</p>
+			</div>
 			<div className={styles.collection}>
-				<div className={styles.cover}>
-					<PictureUploader uploadType='cover' />
-				</div>
+				<section className={styles['photos-container']}>
+					<div className={styles.left}>
+						<h2 className={styles.h2}>Cover Photo</h2>
+						<div className={styles.cover}>
+							<PictureUploader uploadType='cover' />
+						</div>
+					</div>
+					<div className={styles.right}>
+						<h2 className={styles.h2}>Collection Photos</h2>
+						<div className={styles.photos}>
+							(If more than 4 photos, carousel)
+							<div className={styles.grid}>
+								<div className={styles.photo}></div>
+								<div className={styles.photo}></div>
+								<div className={styles.photo}></div>
+								<div className={styles.photo}></div>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section className={styles['yoyos-container']}>
+					<h2 className={styles.h2}>Yoyos</h2>
+					<div className={styles.sort}>
+						<div className={styles.style}>Photos Only</div>
+						<div className={styles.style}>Details Only</div>
+						<div className={styles.style}>Photos and Details</div>
+					</div>
+					<div className={styles.yoyos}>
+						<div className={styles.tile}>
+							(This will be its own component called YoyoTile)
+						</div>
+						<div className={styles.tile}>
+							(This will be its own component called YoyoTile)
+						</div>
+						<div className={styles.tile}>
+							(This will be its own component called YoyoTile)
+						</div>
+						<div className={styles.tile}>
+							(This will be its own component called YoyoTile)
+						</div>
+					</div>
+				</section>
 			</div>
 			<Link href={settingsLink} className={styles['settings-box']}>
 				<Edit className={styles['settings-icon']} />
