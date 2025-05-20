ad1075cbb6b63d9663e980b86a2ae31c4dcc2049
evan-baron <egbaron@gmail.com>
2025-05-20 09:26:01 -0600

    removed profile button from mycollections page

--- src/app/mycollections/page.jsx
+++ src/app/mycollections/page.jsx
@@ -1,17 +1,9 @@
 // Libraries
 import React from 'react';
-import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
-import sessionService from '@/services/sessionService';
-import Link from 'next/link';
-import dayjs from 'dayjs';
 import { validateAndExtendSession } from '@/lib/auth/validateAndExtend';
 
 // Styles
 import styles from './myCollectionsPage.module.scss';
-
-// MUI
-import { West } from '@mui/icons-material';
 
 // Components
 import CollectionsTiles from '../components/collectionsComponents/myCollectionsComponents/collectionsTiles/CollectionsTiles';
@@ -25,10 +17,6 @@ async function MyCollections() {
 				<h1 className={styles.h1}>My Collections</h1>
 				<CollectionsTiles collectionType={'user'} />
 			</div>
-			<Link href='/profile' className={styles['settings-box']}>
-				<West className={styles['settings-icon']} sx={{ fontSize: 30 }} />
-				<p className={styles.settings}>Profile</p>
-			</Link>
 		</div>
 	);
 }
